7ebd85b59694ad8d5235e28c39093767ebfb5a5b
Lvps <805697518@qq.com>
2020-06-03 00:58:58 +0800

    update

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -1,30 +1,19 @@
 const https = require('https')
+const axios = require('axios')
+
+// personal api key: YEDT80X4O75WIFFI
 
 const getData = async (ctx, next) => {
   const req = ctx.request.query
-  const resData = {}
-  await https.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo', res => {
-    // console.log(res)
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
-        resData = parsedData
-  
-        ctx.status = 200
-        ctx.body = {
-          data: parsedData,
-          code: 000,
-          message: 'success'
-        }
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  })
+  const resData = await axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
+  const { data } = resData
+  // console.log(resData.data)
+  ctx.status = 200
+  ctx.body = {
+    data,
+    code: 000,
+    message: 'success'
+  }
 }
 
 module.exports = {
