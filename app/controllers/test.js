b85c995bd4b4c3436391b5fd3d813f8ffe082cbb
Lvps <805697518@qq.com>
2020-06-04 01:19:31 +0800

    update

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -1,10 +1,12 @@
 const https = require('https')
 const axios = require('axios')
+const flatted = require('flatted')
 
 // personal api key: YEDT80X4O75WIFFI
 
 const getData = async (ctx, next) => {
   const req = ctx.request.query
+  console.log(req)
   const resData = await axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
   const { data } = resData
   // console.log(resData.data)
@@ -16,6 +18,19 @@ const getData = async (ctx, next) => {
   }
 }
 
+const getCompany = async (ctx, next) => {
+  const resData = await axios.get('http://nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt')
+  
+  console.log(flatted.stringify(resData))
+  ctx.status = 200
+  ctx.body = {
+    data: flatted.stringify(resData),
+    code: 000,
+    message: 'success'
+  }
+}
+
 module.exports = {
-  getData
+  getData,
+  getCompany
 }
--- routes/test.js
+++ routes/test.js
@@ -4,5 +4,6 @@ const router = new Router()
 const test_controller = require('../app/controllers/test')
 
 router.get('/test', test_controller.getData)
+router.get('/inc', test_controller.getCompany)
 
 module.exports = router
